c8114e50db921ceeafe9bf241e7f50f00f601d78
Leo (Xinyu) Wang <xinyuwang.dev@gmail.com>
2026-07-17 15:36:10 +0100

    create spring boot project and add mysql

---
+++ src/test/java/com/xinyu/esportsticketing/EsportsTicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xinyu.esportsticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsportsTicketingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
