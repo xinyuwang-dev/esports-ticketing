2427074494b8e02d6ba8ef30a4d87bee82f643f7
Leo (Xinyu) Wang <xinyuwang.dev@gmail.com>
2026-07-21 13:10:19 +0100

    feat: create UserRepository for database connection

--- src/main/java/com/xinyu/esportsticketing/repository/UserRepository.java
+++ src/main/java/com/xinyu/esportsticketing/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.xinyu.esportsticketing.repository;
 
-public interface UserRepository {
+import com.xinyu.esportsticketing.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
 }
