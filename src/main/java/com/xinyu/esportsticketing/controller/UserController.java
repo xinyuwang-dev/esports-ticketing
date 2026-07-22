dfc8f1aca85f39f8ab6ae69bbdaa087cded49c1c
Leo (Xinyu) Wang <xinyuwang.dev@gmail.com>
2026-07-22 15:47:50 +0100

    feat: add UserService and UserController to fetch users

--- src/main/java/com/xinyu/esportsticketing/controller/UserController.java
+++ src/main/java/com/xinyu/esportsticketing/controller/UserController.java
@@ -1,4 +1,23 @@
 package com.xinyu.esportsticketing.controller;
 
+import com.xinyu.esportsticketing.entity.User;
+import com.xinyu.esportsticketing.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
 }
--- src/main/java/com/xinyu/esportsticketing/service/UserService.java
+++ src/main/java/com/xinyu/esportsticketing/service/UserService.java
@@ -1,4 +1,18 @@
 package com.xinyu.esportsticketing.service;
 
+import com.xinyu.esportsticketing.entity.User;
+import com.xinyu.esportsticketing.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 }
